68a8649e91cb371b89e89cfeb7a201024659f39e
Andrew Shkimba <andrew.shkimba@gmail.com>
2013-07-17 09:23:22 +0300

    Team model & repository

---
+++ Bs.Calendar.DataAccess/Bases/RepoTeam.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Bs.Calendar.DataAccess.Bases
+{
+    public class RepoTeam : IDisposable
+    {
+        private CalendarContext _context;
+        private TeamRepository _team;
+
+        public TeamRepository Team
+        {
+            get { return _team ?? (_team = new TeamRepository(getContext())); }
+        }
+
+        private CalendarContext getContext()
+        {
+            return _context ?? (_context = new CalendarContext());
+        }
+
+        public void Dispose()
+        {
+            if (_context != null)
+            {
+                _context.Dispose();
+            }
+        }
+    }
+}
---
+++ Bs.Calendar.Models/Team.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Bs.Calendar.Models.Bases;
+
+namespace Bs.Calendar.Models
+{
+    public class Team : BaseEntity
+    {
+        public int TeamID { get; set; }
+        public string name { get; set; }
+    }
+}
