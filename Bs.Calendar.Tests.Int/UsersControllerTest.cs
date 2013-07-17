4e5801b15437f861260a49ec70f7cc990e5c2398
artem713 <art.trubitsyn@gmail.com>
2013-07-17 11:34:40 +0300

    Тест не работает..

---
+++ Bs.Calendar.Tests.Int/UsersControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using Bs.Calendar.DataAccess.Bases;
+using Bs.Calendar.Models;
+using Bs.Calendar.Mvc.Services;
+using Bs.Calendar.Mvc.ViewModels;
+using NUnit.Framework;
+
+namespace Bs.Calendar.Tests.Int
+{
+    [TestFixture]
+    class UsersControllerTest
+    {
+        private RepoUnit _unit;
+
+        [TestFixtureSetUp]
+        public void Setup(RepoUnit unit)
+        {
+            _unit = unit;
+        }
+
+        [Test]
+        public void ShouldAddNewUserToTheDb()
+        {
+            // arrange 
+            var newUser = new User
+                {
+                    Email = "newuser@gmail.com",
+                    FirstName = "New",
+                    LastName = "User",
+                    Role = Roles.Simple
+                };
+
+            var userService = new UserService(_unit);
+            var quantaty = userService.GetAllUsers().Count();
+            
+            // act
+            userService.SaveUser(new UserEditVm(newUser));
+
+            // assert
+            Assert.AreEqual(userService.GetAllUsers().Count(), quantaty);
+        }
+    }
+}
