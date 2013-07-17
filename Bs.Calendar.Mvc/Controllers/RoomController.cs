348b532611a5ebbc138b7f9e7723073475e959ed
PorfiroffPaul <Porfirov.Pavel@mail.ru>
2013-07-17 11:06:49 +0300

    Mended Room model. Improved RoomService

--- Bs.Calendar.Mvc/Controllers/RoomController.cs
+++ Bs.Calendar.Mvc/Controllers/RoomController.cs
@@ -6,17 +6,30 @@ using System.Web;
 using System.Web.Mvc;
 using Bs.Calendar.Mvc.Services;
 
+using Bs.Calendar.Mvc.ViewModels;
+
 namespace Bs.Calendar.Mvc.Controllers
 {
     public class RoomController : Controller
     {
+        private RoomService _rsService;
+
         //
         // GET: /Room/
         public ActionResult Index()
         {
-            RoomService rsService = new RoomService();  /* Инициализация сервиса */
+            _rsService = new RoomService();  /* Инициализация сервиса */
 
-            return View(rsService.CreateView());
+            return View(_rsService.Room);
+        }
+
+        //
+        // GET: /Room/Save
+        public ActionResult Save(RoomEditVm revView)
+        {
+            _rsService.Save(revView);
+
+            return View("Index");
         }
 
     }
