f6e53a8e40852ffa7cf1796f6419ed4c2ac6aa60
Roman Nofenko <rnofenko@gmail.com>
2013-07-19 20:35:23 +0300

    removed bootstrap

--- Bs.Calendar.Mvc/Server/BundleConfig.cs
+++ Bs.Calendar.Mvc/Server/BundleConfig.cs
@@ -1,58 +1,26 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
-namespace Bs.Calendar.Mvc
+namespace Bs.Calendar.Mvc.Server
 {
     public class BundleConfig
     {
-        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+            var scripts = new Bundle("~/Scripts/all", new JsMinify());
+            scripts.Include("~/Scripts/Rooms/Utilities.js");
+            scripts.Include("~/Scripts/Rooms/ColorPicker.js");
+            scripts.Include("~/Scripts/Rooms/SetupEvents.js");
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+            scripts.Include("~/Scripts/Users/user.js");
+            BundleTable.Bundles.Add(scripts);
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryajax").Include(
-                        "~/Scripts/jquery.unobtrusive-ajax.min.js"));
-            bundles.Add(new ScriptBundle("~/bundles/jqueryuser").Include(
-                        "~/Scripts/Users/user.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/roomjs").Include(
-                        "~/Scripts/Rooms/Utilities.js",
-                        "~/Scripts/Rooms/ColorPicker.js",
-                        "~/Scripts/Rooms/SetupEvents.js"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-            bundles.Add(new StyleBundle("~/Content/usercss").Include(
-                        "~/Content/Users/User.css"));
-
-            bundles.Add(new StyleBundle("~/Content/roomcss").Include(
-                        "~/Content/Rooms/ColorPicker.css"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+            var css = new Bundle("~/Content/css", new CssMinify());
+            css.Include("~/Content/gumby.css");
+            css.Include("~/Content/ColorPicker.css");
+            BundleTable.Bundles.Add(css);
         }
     }
 }
