906e4b1cb74b7864f7ada6bc94a9159383c578a2
artem713 <art.trubitsyn@gmail.com>
2013-07-23 02:01:02 +0300

    HashLib is NOT correct find hash of the empty string "" using keccak hash function.
    Another extension SHA3 is correct in this part

--- Bs.Calendar.Rules/CryptoProvider.cs
+++ Bs.Calendar.Rules/CryptoProvider.cs
@@ -1,15 +1,31 @@
-﻿namespace Bs.Calendar.Mvc.Services
+﻿using System;
+
+namespace Bs.Calendar.Mvc.Services
 {
     public class CryptoProvider
     {
         public string GetKeccakHash(string password)
         {
-            throw new System.NotImplementedException();
+            var keccak512 = HashLib.HashFactory.Crypto.SHA3.CreateKeccak512();
+            return keccak512.ComputeString(password).ToString();
         }
 
         public string GetSkeinHash(string password)
         {
-            throw new System.NotImplementedException();
+            var skein512 = HashLib.HashFactory.Crypto.SHA3.CreateSkein512();
+            return skein512.ComputeString(password).ToString();
+        }
+    }
+
+    public static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("HI!");
+            var str = "";
+            var crypto = new CryptoProvider();
+            Console.WriteLine(crypto.GetKeccakHash(str));
+            Console.ReadLine();
         }
     }
 }
