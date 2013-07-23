cf78abfe30afb6c587a3e6b0feeb919903a9207b
artem713 <art.trubitsyn@gmail.com>
2013-07-23 14:46:48 +0300

    Created AcountVm.cs (contains props Email and Password)

--- Bs.Calendar.Mvc/ViewModels/AccountVm.cs
+++ Bs.Calendar.Mvc/ViewModels/AccountVm.cs
@@ -1,8 +1,14 @@
-﻿namespace Bs.Calendar.Mvc.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bs.Calendar.Mvc.ViewModels
 {
     public class AccountVm
     {
+        [Required,
+        EmailAddress]
         public string Email { get; set; }
+
+        [Required]
         public string Password { get; set; }
     }
 }
