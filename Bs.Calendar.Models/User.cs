e943ef28f3784c76768102a2a9a838faee502f5a
artem713 <art.trubitsyn@gmail.com>
2013-07-23 11:18:50 +0300

    minor changes in the User.cs class
    User class contains two prop:
    1) PasswordKeccakHash
    2) PasswordMd5Hash

--- Bs.Calendar.Models/User.cs
+++ Bs.Calendar.Models/User.cs
@@ -18,6 +18,6 @@ namespace Bs.Calendar.Models
 
         public string PasswordKeccakHash { get; set; }
 
-        public string PasswordSkeinHash { get; set; }
+        public string PasswordMd5Hash { get; set; }
     }
 }
