735a0bed4097ac82e20251f7ba9ba3a5774355a3
artem713 <art.trubitsyn@gmail.com>
2013-07-24 14:32:09 +0300

    Added test for MembershipProvider -> ShouldReturnCorrectMembershipUserWithGivenEmail()
    changed App.config file to make test work

---
+++ Bs.Calendar.Tests.Int/CalendarMembershipProviderTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Web.Security;
+using Bs.Calendar.Core;
+using Bs.Calendar.DataAccess;
+using Bs.Calendar.Models;
+using Bs.Calendar.Mvc.Server;
+using Bs.Calendar.Mvc.Services;
+using Bs.Calendar.Mvc.ViewModels;
+using Bs.Calendar.Tests.Unit.FakeObjects;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Bs.Calendar.Tests.Int
+{
+    [TestFixture]
+    public class CalendarMembershipProviderTest
+    {
+        private CalendarMembershipProvider _membershipProvider;        
+        private RepoUnit _unit;
+        private User _user;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            DiMvc.Register();
+            Resolver.RegisterType<IUserRepository, FakeUserRepository>();
+            _unit = Resolver.Resolve<RepoUnit>();
+            _membershipProvider = new CalendarMembershipProvider();
+            _user = new User
+            {
+                Email = "holmes@email.com",
+                FirstName = "Sherlock", LastName = "Holmes",
+                PasswordKeccakHash = "keccak", PasswordMd5Hash = "mdfive",
+                Role = Roles.Simple
+            };            
+            _unit.User.Save(_user);           
+        }
+
+        [Test]
+        public void ShouldReturnCorrectMembershipUserWithGivenEmail()
+        {
+            // act
+            var user = _membershipProvider.GetUser(_user.Email, true);
+
+            // assert
+            user.Should().NotBeNull();
+            user.UserName.Should().Be(string.Format("{0} {1}", _user.FirstName, _user.LastName));
+            user.Email.Should().Be(_user.Email);           
+        }
+
+        [Test]
+        public void 
+    }
+}
