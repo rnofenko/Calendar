f2982e339510ef99c50600ad9ab672edfc4ca257
Anton Priadko <d.ark.3.1415@gmail.com>
2013-07-25 08:36:51 +0300

    Implemented async message sender

--- Bs.Calendar.Rules/EmailSender.cs
+++ Bs.Calendar.Rules/EmailSender.cs
@@ -1,21 +1,48 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Mail;
+using System.Threading;
 
 namespace Bs.Calendar.Rules
 {
     public class EmailSender
     {
-        public static void SendEmail(MailMessage message)
+        private const int ThreadsCount = 10;
+
+        private class AsyncSender
         {
-            message.From = new MailAddress("binary.calendar@gmail.com", "Binary Calendar");
-            var smtp = new SmtpClient
+            public void Send(object threadContext)
             {
-                Port = 587,
-                Host = "smtp.gmail.com",
-                Credentials = new NetworkCredential("binary.calendar", "binarystudio"),
-                EnableSsl = true
-            };
-            smtp.Send(message);
+                Console.WriteLine("Thread starts");
+                var message = threadContext as MailMessage;
+                if (message == null)
+                {
+                    return;
+                }
+                message.From = new MailAddress("binary.calendar@gmail.com", "Binary Calendar");
+                var smtp = new SmtpClient
+                    {
+                        Port = 587,
+                        Host = "smtp.gmail.com",
+                        Credentials = new NetworkCredential("binary.calendar", "binarystudio"),
+                    };
+                smtp.EnableSsl = true;
+                smtp.Send(message);
+                Console.WriteLine("Message is sent");
+            }
+        }
+
+        public EmailSender()
+        {
+            Console.WriteLine(ThreadPool.SetMaxThreads(ThreadsCount, ThreadsCount));
+        }
+
+        public void SendEmail(MailMessage message)
+        {
+            var sender = new AsyncSender();
+            Console.WriteLine("Ready");
+            ThreadPool.QueueUserWorkItem(sender.Send, message);
+            Console.WriteLine("Queued");
         }
     }
 }
--- Bs.Calendar.Tests.Int/EmailTest.cs
+++ Bs.Calendar.Tests.Int/EmailTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Mail;
+using System.Threading;
 using Bs.Calendar.Rules;
 using NUnit.Framework;
 
@@ -11,12 +12,14 @@ namespace Bs.Calendar.Tests.Int
         // this is interactive test. If it succed, you should check you email, if you got a lettert
         public void Can_send_test_email()
         {
-            var email = "d.ark.3.1415@gmail.com";  
+            const string email = "d.ark.3.1415@gmail.com";  
             var message = new MailMessage();
             message.To.Add(email);
             message.Subject = "Test Mail";
             message.Body = "This is the message body";
-            EmailSender.SendEmail(message);            
+            var sender = new EmailSender();
+            sender.SendEmail(message);
+            Thread.Sleep(10000);    
         }
     }
 }
