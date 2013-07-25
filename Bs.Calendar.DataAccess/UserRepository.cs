9a7923a73c258ad60bdd867ae5ab53e457f1c039
artem713 <art.trubitsyn@gmail.com>
2013-07-25 23:36:49 +0300

    overrided Load() method in the UserRepository

--- Bs.Calendar.DataAccess/UserRepository.cs
+++ Bs.Calendar.DataAccess/UserRepository.cs
@@ -1,9 +1,14 @@
-﻿using Bs.Calendar.DataAccess.Bases;
+﻿using System.Linq;
+using Bs.Calendar.DataAccess.Bases;
 using Bs.Calendar.Models;
 
 namespace Bs.Calendar.DataAccess
 {
     public class UserRepository : BaseRepository<User>, IUserRepository
     {
+        public new IQueryable<User> Load()
+        {
+            return Load(u => u.LiveState == LiveState.Ok);
+        }
     }
 }
