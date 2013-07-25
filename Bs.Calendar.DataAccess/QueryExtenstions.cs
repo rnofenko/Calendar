24028415f510222f1251698c2f4221c28830a9c4
Roman Nofenko <rnofenko@gmail.com>
2013-07-25 19:23:22 +0300

    added extension

---
+++ Bs.Calendar.DataAccess/QueryExtenstions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bs.Calendar.DataAccess
+{
+    public static class QueryExtenstions
+    {
+        public static IEnumerable<TSource> WhereIf<TSource>(this IEnumerable<TSource> source, bool condition, Func<TSource, bool> predicate)
+        {
+            if (condition)
+                return source.Where(predicate);
+            return source;
+        }
+    }
+}
