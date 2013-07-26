76d6b29e0d988b6de30aa82a9c6d40a25222eb65
artem713 <art.trubitsyn@gmail.com>
2013-07-26 19:46:51 +0300

    Implemented "Birthday for month" task

--- Bs.Calendar.Mvc/Services/HomeService.cs
+++ Bs.Calendar.Mvc/Services/HomeService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Bs.Calendar.DataAccess;
 using Bs.Calendar.Models;
@@ -11,14 +12,19 @@ namespace Bs.Calendar.Mvc.Services
         {
             using (var unit = new RepoUnit())
             {
-                var users = unit.User.Load().ToList();
+                var users = unit.User.Load(u => u.BirthDate.Month == DateTime.Now.Month && u.BirthDate.Day > DateTime.Now.Day && u.LiveState == LiveState.Ok).ToList();
 
                 if (!users.Any())
                 {
-                    unit.User.Save(new User {Email = "rnofenko@gmail.com", Role = Roles.Admin, LiveState = LiveState.Ok,
-                                             FirstName = "Roman",
-                                             LastName = "Nofenko",
-                                             PasswordKeccakHash = "E9447A0B454AA39752445D6DCD2619F25C83F6453BA463C614820239CDC7CAB811F0C75D27776E119836523CF839C90596F2C0B07A45023741C200B51B6944D4"
+                    unit.User.Save(new User
+                    {
+                        Email = "rnofenko@gmail.com",
+                        Role = Roles.Admin,
+                        LiveState = LiveState.Ok,
+                        FirstName = "Roman",
+                        LastName = "Nofenko",
+                        PasswordKeccakHash = "E9447A0B454AA39752445D6DCD2619F25C83F6453BA463C614820239CDC7CAB811F0C75D27776E119836523CF839C90596F2C0B07A45023741C200B51B6944D4",
+                        BirthDate = new DateTime(1991,7,27)
                     });
                     unit.User.Save(new User
                     {
@@ -27,11 +33,11 @@ namespace Bs.Calendar.Mvc.Services
                         LiveState = LiveState.Ok,
                         FirstName = "Artem",
                         LastName = "Trubitsyn",
-                        PasswordKeccakHash = "4D14E3527467EA4AF5983EFF0C3FEECB48E7230EE7EAC0BA793B3B53B5BB6D1259E33C3ECA7B2A29AF4943391B913B8432F587D74D006D0AD9F3BFB3E8C4871F"
+                        PasswordKeccakHash = "4D14E3527467EA4AF5983EFF0C3FEECB48E7230EE7EAC0BA793B3B53B5BB6D1259E33C3ECA7B2A29AF4943391B913B8432F587D74D006D0AD9F3BFB3E8C4871F",
+                        BirthDate = new DateTime(1991,07,29)
                     });
                     users = unit.User.Load().ToList();
-                }
-
+                }              
                 return users;
             }
         }
