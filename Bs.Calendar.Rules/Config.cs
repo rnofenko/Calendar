3dc742ad483978f7200a121d5abedb40a468651b
StayCool <tobondini@gmail.com>
2013-07-29 23:27:15 +0300

    Added Config class, updated SendEmail

---
+++ Bs.Calendar.Rules/Config.cs
@@ -0,0 +1,14 @@
+﻿using Bs.Calendar.Core;
+
+namespace Bs.Calendar.Rules
+{
+    public class Config
+    {
+        private static readonly IConfig _config = Resolver.Resolve<IConfig>();
+
+        public static IConfig Instance
+        {
+            get { return _config; }
+        }
+    }
+}
