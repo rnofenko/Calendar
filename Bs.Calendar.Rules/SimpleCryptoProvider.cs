3f8064b603f8850c470ef00aaa5dfe3f4b301f13
artem713 <art.trubitsyn@gmail.com>
2013-08-02 15:12:06 +0300

    added SimpleCryptoProvider

---
+++ Bs.Calendar.Rules/SimpleCryptoProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Bs.Calendar.Rules
+{
+    class SimpleCryptoProvider : ICryptoProvider
+    {
+        public string GetHash(string data)
+        {
+            return data;
+        }
+
+        public string GetHashWithSalt(string data, string salt="")
+        {
+            return String.IsNullOrEmpty(salt) ? data : string.Format("{0}{1}", data, salt);
+        }
+    }
+}
