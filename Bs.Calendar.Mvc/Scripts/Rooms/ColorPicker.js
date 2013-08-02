8e8200af2dd30f9a91a391c2a6ae9f79e69d5fe1
Anton Priadko <d.ark.3.1415@gmail.com>
2013-08-02 10:02:04 +0300

    Moved styles and scripts to layout

--- Bs.Calendar.Mvc/Scripts/Rooms/ColorPicker.js
+++ Bs.Calendar.Mvc/Scripts/Rooms/ColorPicker.js
@@ -3,7 +3,3 @@ select_color = function (color) {
     $("#color_" + String(color)).toggleClass('roomColorSelected');
     $("#Color").val(color);
 }
-
-$(document).ready(function() {
-    $('#NumberOfPlaces').toggleClass("wide text input pull_right");
-})
