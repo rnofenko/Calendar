8bc3c3d4fa12a77270c29757f23ba5e16dd32b48
Anton Priadko <d.ark.3.1415@gmail.com>
2013-08-05 07:28:54 +0300

    Added service and controller

---
+++ Bs.Calendar.Mvc/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Bs.Calendar.Models;
+using Bs.Calendar.Mvc.Services;
+
+namespace Bs.Calendar.Mvc.Controllers
+{
+    public class BookController : ApiController
+    {
+        private readonly BookService _service;
+
+        public BookController(BookService service)
+        {
+            _service = service;
+        }
+
+        // GET: /Api/Book/
+        public List<Book> Get()
+        {
+            return _service.List();
+        }
+
+    }
+}
---
+++ Bs.Calendar.Mvc/Services/BookService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Bs.Calendar.DataAccess;
+using Bs.Calendar.Models;
+
+namespace Bs.Calendar.Mvc.Services
+{
+    public class BookService
+    {
+        private readonly RepoUnit _repoUnit;
+
+        public BookService(RepoUnit repository)
+        {
+            _repoUnit = repository;
+        }
+
+        public Book Get(int id)
+        {
+            var book = _repoUnit.Book.Get(id);
+            return book;
+        }
+
+        public List<Book> List()
+        {
+            var books = _repoUnit.Book.Load();
+            return books.ToList();
+        }
+
+    }
+}
