4addbbc65ad0b2bf84e9d64e0f332696a934a831
artem713 <art.trubitsyn@gmail.com>
2013-08-06 14:56:28 +0300

    added tests for ContactTypeParser

--- Bs.Calendar.Tests.Unit/ContactTypeParserTest.cs
+++ Bs.Calendar.Tests.Unit/ContactTypeParserTest.cs
@@ -10,6 +10,10 @@ namespace Bs.Calendar.Tests.Unit
     {
         [TestCase("rnofenko@gmail.com", Result = ContactType.Email)]
         [TestCase("art.trubitsyn@gmail.com", Result = ContactType.Email)]
+        [TestCase("art trubitsyn@gmail.com", Result = ContactType.None)]
+        [TestCase("r@nofenko@gmail.com", Result = ContactType.None)]
+        [TestCase("roman.@gmail.com", Result = ContactType.None)]
+        [TestCase("ro@.gmail.com", Result = ContactType.None)]
         public ContactType ContactParserShouldReturnEmailTypeIfInputStringIsEmail(string inputString)
         {
             // act && assert
@@ -18,8 +22,13 @@ namespace Bs.Calendar.Tests.Unit
 
         [TestCase("@rnofenko", Result = ContactType.Twitter)]
         [TestCase("@art713", Result = ContactType.Twitter)]
+        [TestCase("@   art713", Result = ContactType.None)]
+        [TestCase("@06082013", Result = ContactType.Twitter)]
+        [TestCase("@___", Result = ContactType.Twitter)]
+        [TestCase("@maxisfifteensymb", Result = ContactType.None)]
         public ContactType ContactParserShouldReturnTwitterTypeIfInputStringIsTwitterAccount(string inputString)
         {
+            // http://kagan.mactane.org/blog/2009/09/22/what-characters-are-allowed-in-twitter-usernames/
             // act && assert
             return ContactTypeParser.GetContactType(inputString);
         }
@@ -27,6 +36,10 @@ namespace Bs.Calendar.Tests.Unit
         [TestCase("r_nofenko", Result = ContactType.Skype)]
         [TestCase("art713", Result = ContactType.Skype)]
         [TestCase("j*hn-nash", Result = ContactType.None)]
+        [TestCase("nash", Result = ContactType.None)]
+        [TestCase("tweebuffelsmeteenskootmorsdoodgeskietfontein", Result = ContactType.None)]
+        [TestCase("713art", Result = ContactType.None)]
+        [TestCase("roman nofenko", Result = ContactType.None)]
         public ContactType ContactParserShouldReturnSkypeTypeIfInputStringIsSkypeAccount(string inputString)
         {
             // act && assert
@@ -39,6 +52,9 @@ namespace Bs.Calendar.Tests.Unit
         [TestCase("050-234-26-71", Result = ContactType.Phone)]
         [TestCase("050-23-42-671", Result = ContactType.Phone)]
         [TestCase("(050)2342671", Result = ContactType.Phone)]
+        [TestCase("050 234 26 71", Result = ContactType.Phone)]
+        [TestCase("050 23 42 671", Result = ContactType.Phone)]
+        [TestCase("+38 050 23 42 671", Result = ContactType.Phone)]
         public ContactType ContactParserShouldReturnPhoneTypeIfInputStringIsPhoneNumber(string inputString)
         {
             // act && assert
@@ -50,6 +66,8 @@ namespace Bs.Calendar.Tests.Unit
         [TestCase("art713.gov.us", Result = ContactType.Url)]
         [TestCase("https://gmail.com", Result = ContactType.Url)]
         [TestCase("http://facebook.com", Result = ContactType.Url)]
+        [TestCase("binary studio.com", Result = ContactType.None)]
+        [TestCase("binary%20studio.com", Result = ContactType.Url)]
         public ContactType ContactParserShouldReturnUrlTypeIfInputStringIsUrl(string inputString)
         {
             // act && assert
