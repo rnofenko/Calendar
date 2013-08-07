c8cb9e1c6d6eee51de3f2b5f96ef00b1d4baea24
Anton Priadko <d.ark.3.1415@gmail.com>
2013-08-08 02:25:46 +0300

    Added paging to knockout vm, added bindings

--- Bs.Calendar.Mvc/Scripts/Books/BooksVm.js
+++ Bs.Calendar.Mvc/Scripts/Books/BooksVm.js
@@ -1,9 +1,56 @@
-﻿window.BooksVm = function ()
+﻿var isNumber = function(x) {
+    return !isNaN(x - 0);
+};
+
+window.BooksVm = function ()
 {
     var self = this;
 
-    self.page = ko.observable(1);
     self.TotalPages = ko.observable(1);
+    self._page = ko.observable(1);
+    self.isCorrectPage = function (page) {
+        return page == 1 || (isNumber(page) && page > 0 && page <= self.TotalPages());
+    };
+    self.page = ko.computed({
+        write: function (value) {
+            if (value == self._page()) {
+                self._page("---");
+                self.page(value);
+                return;
+            }
+            if (!self.isCorrectPage(value)) {
+                if (!self.isCorrectPage(self._page())) {
+                    self._page(1);
+                }
+                self.page(self._page());
+                return;
+            }
+            self._page(value);
+            self.LoadData();
+        },
+        read: function () {
+            return self._page();
+        },
+        owner: this
+    });
+    self.nextPage = function() {
+        self.page(self.page() - 0 + 1);
+    };
+    self.prevPage = function() {
+        self.page(self.page() - 1);
+    };
+    self.shouldShowNextPage = ko.computed(function () {
+        console.log(self);
+        return self.page() < self.TotalPages();
+    });
+    self.shouldShowPrevPage = ko.computed(function () {
+        console.log(self);
+        return self.page() > 1;
+    });
+    self.shouldShowPager = ko.computed(function () {
+        console.log(self);
+        return self.TotalPages() > 1;
+    });
 
     self._searchStr = "";
     self.searchStr = ko.computed({
@@ -49,7 +96,6 @@
     {
         self.books.removeAll();
         self.TotalPages(data['TotalPages']);
-        self.page(data['CurrentPage']);
         data = data['Data'];
         for (var i = 0; i < data.length; ++i)
         {
@@ -61,8 +107,9 @@
     {
         var data = {
             orderby: self._orderby,
-            page: self.page
+            page: self.page()
         };
+        console.log(data);
         if (self._searchStr != "")
         {
             data['search'] = self._searchStr;
