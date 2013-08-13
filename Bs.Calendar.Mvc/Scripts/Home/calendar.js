7f51afaed5a37e4a3669dec9f7561331453e76be
Anton Priadko <d.ark.3.1415@gmail.com>
2013-08-13 09:01:08 +0300

    Calendar with fixed events created

--- Bs.Calendar.Mvc/Scripts/Home/calendar.js
+++ Bs.Calendar.Mvc/Scripts/Home/calendar.js
@@ -1,11 +1,18 @@
 ﻿
-window.DayVm = function (day_title) {
+window.EventVm = function(event_content, event_type){
+    self = this;
+    self.event_content = event_content;
+    self.event_type = event_type;
+};
+window.DayVm = function (day_title, events) {
     var self = this;
-    self.day_title = ko.observable(day_title);
+    self.day_title = day_title;
+    self.events = ko.observableArray(events || []);
 };
 window.WeekVm = function(days) {
     var self = this;
-    self.days = ko.observableArray(days);
+    days.unshift(new DayVm(""));
+    self.days = days;
 };
 window.MonthVm = function() {
     var self = this;
@@ -15,13 +22,21 @@ window.MonthVm = function() {
             new DayVm("1"),
             new DayVm("2"),
             new DayVm("3"),
-            new DayVm("4"),
+            new DayVm("4", [
+                    new EventVm("Event 1", "alert"),
+                    new EventVm("Event 2", "alert")
+                ]),
             new DayVm("5"),
             new DayVm("6")
         ]),
         new WeekVm([
             new DayVm("7"),
-            new DayVm("8"),
+            new DayVm("8", [
+                    new EventVm("Event 1", "alert"),
+                    new EventVm("Event 2", "alert"),
+                    new EventVm("Event 3", "alert"),
+                    new EventVm("Event 4", "alert")
+                ]),
             new DayVm("9"),
             new DayVm("10"),
             new DayVm("11"),
