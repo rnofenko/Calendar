19be3a19da1b0f76df3e1f32bf2e2ce69beeb8b0
StayCool <tobondini@gmail.com>
2013-08-15 17:36:17 +0300

    Minor calendar updates

--- Bs.Calendar.Models/BaseEvent.cs
+++ Bs.Calendar.Models/BaseEvent.cs
@@ -3,9 +3,14 @@
 namespace Bs.Calendar.Models
 {
     public class BaseEvent
-    {        
+    {
+        public string Theme { get; set; }
         public string Text { get; set; }
+        public Room Place { get; set; }
 
-        public DateTime Date { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+
+        public bool IsAllDay { get; set; }
     }
 }
